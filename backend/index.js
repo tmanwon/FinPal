4fefb377105375c7987f34c30ff9b592d1751507
Matt Mocklin <matt.mocklin@gmail.com>
2025-02-08 20:38:25 -0500

    openAI backend setup

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Allow requests from frontend
+app.use(cors()); 
 
 const plaidRoutes = require("./routes/plaidRoutes");
+const openaiRoutes = require("./routes/openaiRoutes"); // Import OpenAI routes
+
+
 app.use("/plaid", plaidRoutes);
+app.use("/openai", openaiRoutes);
 
 app.get("/", (req, res) => {
     res.send("Backend is working!");
